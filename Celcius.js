4f65aca12f95b94cf7394a9447d8159b1a199108
Enes Özmen <doclock4715@windowslive.com>
2022-06-11 15:37:30 +0300

    Delete Celcius.js

--- Celcius.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Celcius extends Component {
-  render() {
-    return (
-      <div>Celcius: {this.props.currentSicaklik}</div>
-    )
-  }
-}
