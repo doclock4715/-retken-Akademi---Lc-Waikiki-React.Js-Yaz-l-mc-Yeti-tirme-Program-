5c4525a1166efd85288550c3fede674b81cb92d1
Enes Özmen <doclock4715@windowslive.com>
2022-06-11 15:37:35 +0300

    Delete Fahrenheit.js

--- Fahrenheit.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Fahrenheit extends Component {
-  render() {
-    return (
-      <div>Fahrenheit: {this.props.currentSicaklik}</div>
-    )
-  }
-}
