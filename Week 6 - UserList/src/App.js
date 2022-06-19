b154a92372bbdd99ef716ad91a62f8b4544ae3f7
Enes Özmen <doclock4715@windowslive.com>
2022-06-19 20:18:17 +0300

    Add files via upload

---
+++ Week 6 - UserList/src/App.js
@@ -0,0 +1,56 @@
+
+import React, { Component } from "react";
+import axios from "axios";
+
+
+export default class App extends Component {
+  state = {
+    people: [],
+    loading: false,
+    url: "https://jsonplaceholder.typicode.com/users"
+  }
+
+  componentDidMount() {
+    axios.get(`${this.state.url}`)
+      .then(res => {
+        const people = res.data;
+        this.setState({
+          people: people,
+          loading: true
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div>{
+        (this.state.loading === true) ? <ul>
+          {
+            this.state.people
+              .map(person =>
+                <li key={person.id}>{person.username}:{person.name}</li>
+              )
+          }
+        </ul> : <p>Loading...</p>
+      }
+      </div>
+    );
+    /*     if (this.state.loading === true) {
+          return (
+            <ul>
+              {
+                this.state.people
+                  .map(person =>
+                    <li key={person.id}>{person.username}:{person.name}</li>
+                  )
+              }
+            </ul>
+          )
+        }
+        else {
+          return (
+            <p>Loading...</p>
+          )
+        } */
+  }
+}
